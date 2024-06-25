47a957e3fd50e2541c342dfc75458ed80dd6e1d5
Bhanudeep Simhadri <bhanudeepsimhadry@gmail.com>
2024-06-25 21:55:49 +0530

    Update autoJobApplierLinkedIn.py

--- autoJobApplierLinkedIn.py
+++ autoJobApplierLinkedIn.py
@@ -7,7 +7,6 @@ Copyright (C) 2024 Bhanudeep Simhadri
 License:    GNU Affero General Public License
             https://www.gnu.org/licenses/agpl-3.0.en.html
             
-GitHub:     https://github.com/GodsScion/Auto_job_applier_linkedIn
 
 '''
 
